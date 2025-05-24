401566a691f5cb57434a279fd52b9f4b27777b00
Eric Roos <ericroos@hey.com>
2025-05-24 15:22:47 -0500

    add distinct author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Current.user.authors.includes(books: [ cover_image_attachment: [ :blob ] ]).order(:name)
+    @authors = Current.user.authors.includes(books: [ cover_image_attachment: [ :blob ] ]).distinct.order(:name)
   end
 end

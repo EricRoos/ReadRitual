dcd1d9422118362d9dfdaf47428185366f41d6c4
Eric Roos <ericroos@hey.com>
2025-05-23 19:54:32 -0500

    add system tests

---
+++ test/system/create_book_from_dashboards_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class CreateBookFromDashboardsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit create_book_from_dashboards_url
+  #
+  #   assert_selector "h1", text: "CreateBookFromDashboard"
+  # end
+  #
+  test "create book from dashboard" do
+    user = users(:one)
+    login_as(user)
+    visit root_path
+    click_on "Add a Book"
+    fill_in "Title", with: "Test Book"
+    fill_in "Author", with: "Test Author"
+    fill_in "Start date", with: "01/01/2023"
+    click_on "Create Book"
+    assert_text "Book was successfully created"
+    assert_text "You are currently reading Test Book"
+  end
+end
---
+++ test/system/edit_books_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class EditBooksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit edit_books_url
+  #
+  #   assert_selector "h1", text: "EditBooks"
+  # end
+  test "updating a Book" do
+    book = books(:one)
+    login_as book.user
+    visit edit_book_url(book)
+    fill_in "Title", with: "Updated Title"
+    fill_in "Author", with: "Updated Author"
+    click_on "Update Book"
+    assert_text "Book was successfully updated"
+    book.reload
+    assert_equal "Updated Title", book.title
+    assert_equal "Updated Author", book.authors.first.name
+  end
+end

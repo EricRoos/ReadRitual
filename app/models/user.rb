75d11f8420bbda74153558a73695f3d498a0b0d1
Eric Roos <ericroos@hey.com>
2025-06-02 15:26:19 -0500

    goal calculation changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   def earliest_in_progress = books.merge(Book.in_progress).order(start_date: :asc).first
   def in_progress_books = books.merge(Book.in_progress)
   def completed_books = books.merge(Book.completed)
-  def completed_prior_to_this_month = completed_books.where("finish_date < ?", Time.current.beginning_of_month)
 
   def completed_per_month
     month_counts = completed_books
@@ -18,13 +17,34 @@ class User < ApplicationRecord
     month_counts.values.sum / month_counts.size.to_f
   end
 
+  def completed_per_day
+    return 0 if completed_books_count.zero?
+    completed_books_count / (Date.current - completed_books.minimum(:start_date))
+  end
+
+
+  def in_progress_books_count = in_progress_books.count
+  def completed_books_count = completed_books.count
+
   def in_progress_or_recently_completed = in_progress_books.or(recently_completed)
   def recently_completed = books.merge(Book.recently_completed)
   def books_per_year_goal = 100
+
+  def estimated_completion_date
+    return nil if books_left_in_goal <= 0
+    days_to_finish = books_left_in_goal / completed_per_day
+    Date.current + days_to_finish.days
+  end
 
   def completed_books_count
     Rails.cache.fetch([ self, :completed_books_count ]) do
       completed_books.count
     end
+  end
+
+  protected
+
+  def books_left_in_goal
+    books_per_year_goal - completed_books_count
   end
 end

2e878cc23785f359e88c5c3d6da562d9bb2c92e1
Eric Roos <ericroos@hey.com>
2025-06-25 11:02:26 -0500

    feat: add manual entry for add from audible

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -7,7 +7,21 @@ export default class extends Controller {
   async paste() {
     try {
       // Read text from clipboard directly
-      const clipboardText = await navigator.clipboard.readText();
+      let clipboardText = await navigator.clipboard.readText();
+      if (!clipboardText) {
+        clipboardText = await navigator.clipboard.read();
+        if (clipboardText.length === 0 || !clipboardText[0].types.includes("text/plain")) {
+          clipboardText = "";
+        } else {
+          clipboardText = await clipboardText[0].getType("text/plain").then(blob => blob.text());
+        }
+      }
+
+      if (!clipboardText) {
+        alert("Clipboard is empty or does not contain text.");
+        return;
+      }
+
       this.fieldTarget.value = clipboardText;
       const acceptedHosts = ["www.audible.com"];
       const url = new URL(clipboardText);

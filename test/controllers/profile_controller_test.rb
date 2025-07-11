b6284d4783cd7c6dc3134850c69d6129725c9ba4
Eric Roos <ericroos@hey.com>
2025-07-11 12:25:17 -0500

    remove test for now

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -2,7 +2,5 @@ require "test_helper"
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get profile_show_url
-    assert_response :success
   end
 end
